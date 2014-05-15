70a2112d55c3010af6a18d5186b8a6a0f1560cd2
schwannden <schwannden@140-113-65-15.Dorm7.NCTU.edu.tw>
2014-05-15 17:02:42 +0800

    cast price into int in javascript

--- include/lib.js
+++ include/lib.js
@@ -7,12 +7,12 @@ function tableToArray( tableId, is_admin )
     for( i = 1 ; i < flight_table.length ; i++ )
     {
       result[i-1] = [];
-      result[i-1][0] = flight_table[i].cells[0].firstElementChild.value + " ";
-      result[i-1][1] = flight_table[i].cells[1].firstElementChild.value + " ";
-      result[i-1][2] = flight_table[i].cells[2].firstElementChild.value + " ";
-      result[i-1][3] = flight_table[i].cells[3].firstElementChild.value + " ";
-      result[i-1][4] = flight_table[i].cells[4].firstElementChild.value + " ";
-      result[i-1][5] = flight_table[i].cells[5].firstElementChild.value + " ";
+      result[i-1][0] = flight_table[i].cells[0].firstElementChild.value;
+      result[i-1][1] = flight_table[i].cells[1].firstElementChild.value;
+      result[i-1][2] = flight_table[i].cells[2].firstElementChild.value;
+      result[i-1][3] = flight_table[i].cells[3].firstElementChild.value;
+      result[i-1][4] = flight_table[i].cells[4].firstElementChild.value;
+      result[i-1][5] = parseInt( flight_table[i].cells[5].firstElementChild.value);
     }
   }
   else
