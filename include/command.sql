821f8b13afe681d21ec717dcf4f9dffdf43924a0
schwannden <schwannden@140-113-65-15.Dorm7.NCTU.edu.tw>
2014-05-16 00:52:46 +0800

    user proper time command (subtime instead of '-', and full time format XX:XX:XX instead of XX:XX)

--- include/command.sql
+++ include/command.sql
@@ -4,8 +4,8 @@ select * from
            a2.code as 's3', f1.flight_number as 'third_flight',
            a2.code as 's4', f1.price as 'price', 0 as 'transition',
            f1.departure_date as departure_date, f1.arrival_date as arrival_date,
-           TIMEDIFF( ADDTIME(f1.arrival_date , -a2.timezone),
-             ADDTIME(f1.departure_date, -a1.timezone)) as 'flight_time',
+           TIMEDIFF( SUBTIME(f1.arrival_date , a2.timezone),
+             SUBTIME(f1.departure_date, a1.timezone) ) as 'flight_time',
            '00:00:00' as 'transition_time'
     from Flight as f1, Airport_zone as a1, Airport_zone as a2
     where f1.departure = 'TPE' and f1.destination = 'DSM' and
@@ -17,10 +17,10 @@ select * from
            a3.code as 's4', FLOOR(0.9 * (f1.price + f2.price)) as 'price', 1 as 'transition',
            f1.departure_date as departure_date, f2.arrival_date as arrival_date,
            ADDTIME(
-             TIMEDIFF( ADDTIME(f1.arrival_date , -a2.timezone),
-               ADDTIME(f1.departure_date, -a1.timezone)),
-             TIMEDIFF( ADDTIME(f2.arrival_date , -a3.timezone),
-               ADDTIME(f2.departure_date, -a2.timezone))
+             TIMEDIFF( SUBTIME(f1.arrival_date , a2.timezone),
+               SUBTIME(f1.departure_date, a1.timezone) ),
+             TIMEDIFF( SUBTIME(f2.arrival_date , a3.timezone),
+               SUBTIME(f2.departure_date, a2.timezone) )
            ) as 'flight_time',
            TIMEDIFF( f2.departure_date, f1.arrival_date ) as 'transition_time'
     from Flight  as f1, Flight  as f2,
@@ -28,7 +28,7 @@ select * from
     where f1.departure = 'TPE' and f2.destination = 'DSM' and 
           f1.destination = f2.departure and 
           a1.code = f1.departure and a2.code = f2.departure and a3.code = f2.destination and
-          TIMEDIFF( f2.departure_date, f1.arrival_date ) > '2:00'
+          TIMEDIFF( f2.departure_date, f1.arrival_date ) > '02:00:00'
   UNION
     select a1.code as 's1', f1.flight_number as 'first_flight',
            a2.code as 's2', f2.flight_number as 'second_flight',
@@ -36,12 +36,12 @@ select * from
            a4.code as 's4', FLOOR(0.8 * (f1.price + f2.price + f3.price)) as 'price', 2 as 'transition',
            f1.departure_date as departure_date, f3.arrival_date as arrival_date,
            ADDTIME( ADDTIME(
-             TIMEDIFF( ADDTIME(f1.arrival_date , -a2.timezone ),
-               ADDTIME(f1.departure_date, -a1.timezone )),
-             TIMEDIFF( ADDTIME(f2.arrival_date , -a3.timezone ),
-               ADDTIME(f2.departure_date, -a2.timezone ))),
-             TIMEDIFF( ADDTIME(f3.arrival_date , -a4.timezone ),
-               ADDTIME(f3.departure_date, -a3.timezone ))) as 'flight_time',
+             TIMEDIFF( SUBTIME(f1.arrival_date , a2.timezone ),
+               SUBTIME(f1.departure_date, a1.timezone )),
+             TIMEDIFF( SUBTIME(f2.arrival_date , a3.timezone ),
+               SUBTIME(f2.departure_date, a2.timezone ))),
+             TIMEDIFF( SUBTIME(f3.arrival_date , a4.timezone ),
+               SUBTIME(f3.departure_date, a3.timezone ))) as 'flight_time',
            ADDTIME(
              TIMEDIFF( f2.departure_date, f1.arrival_date ),
              TIMEDIFF( f3.departure_date, f2.arrival_date )
@@ -53,6 +53,6 @@ select * from
           f2.destination = f3.departure and
           a1.code = f1.departure and a2.code = f2.departure and
           a3.code = f3.departure and a4.code = f3.destination and
-          TIMEDIFF( f2.departure_date, f1.arrival_date ) > '2:00' and
-          TIMEDIFF( f3.departure_date, f2.arrival_date ) > '2:00'
+          TIMEDIFF( f2.departure_date, f1.arrival_date ) > '02:00:00' and
+          TIMEDIFF( f3.departure_date, f2.arrival_date ) > '02:00:00'
   ) as TT;
